27c413df0c3f0770530cab544d1c6126026c9b9b
MaahiVaghela <maahivaghela05@gmail.com>
2025-04-30 02:17:32 +0530

    once more

--- src/main.jsx
+++ src/main.jsx
@@ -1,23 +1,21 @@
-// import { StrictMode } from 'react'
-// import { createRoot } from 'react-dom/client'
-// import './index.css'
-// import App from './App.jsx'
-// import { HashRouter as Router } from 'react-router-dom'
-// createRoot(document.getElementById('root')).render(
-//   <StrictMode>
-//     <Router>
-//     <App />
-//     </Router>
-//   </StrictMode>,
-// )
-
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import { BrowserRouter } from 'react-router-dom'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { HashRouter as Router } from 'react-router-dom'
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
     <App />
-  </BrowserRouter>
+  </StrictMode>,
 )
+
+// import React from 'react'
+// import ReactDOM from 'react-dom/client'
+// import App from './App'
+// import { BrowserRouter } from 'react-router-dom'
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <BrowserRouter>
+//     <App />
+//   </BrowserRouter>
+// )
